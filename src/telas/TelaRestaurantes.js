86c840990d1c6a6fb7d57efdcb4dee7c2155244b
Mario Nunes <marionunes_azevedo@hotmail.com>
2024-11-27 20:45:29 -0300

    fetch restaurantes

--- src/telas/TelaRestaurantes.js
+++ src/telas/TelaRestaurantes.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { ScrollView, Text, Image, StyleSheet } from "react-native";
 
 export default function TelaRestaurantes() {
   return (
-    <View style={estilos.container}>
-      <Text style={estilos.titulo}>Restaurantes Recomendados</Text>
-      <Text style={estilos.texto}>- La Trattoria: Culinária italiana autêntica, $$$.</Text>
-      <Text style={estilos.texto}>- Sabor Mineiro: Comida caseira com toque mineiro, $$.</Text>
-      <Text style={estilos.texto}>- Café Gourmet: Cafés e sobremesas deliciosas, $.</Text>
-    </View>
+    <ScrollView style={estilos.container}>
+      <Text style={estilos.titulo}>RESTAURANTES</Text>
+
+      <Text style={estilos.titulo}>A FIGUEIRA</Text>
+      <Image
+        source={require("../../assets/FIGUEIRA.jpg")}
+        style={estilos.principal}
+      />
+      <Text style={estilos.texto}>
+        🍻🍖🔥Restaurante e Petiscaria "A FIGUEIRA", A árvore centenária em
+        IEPÊ-SP que dá o nome ao restaurante 🌳
+      </Text>
+
+      <Text style={estilos.titulo}>PESQUEIRO PAIANOS</Text>
+      <Image
+        source={require("../../assets/pesqueiro.jpg")}
+        style={estilos.principal}
+      />
+      <Text style={estilos.texto}>
+        Pesqueiro e Restaurante Diversão e boa comida Porções 🍤 Frango caipira
+        🍗 Porco no tacho🥘 Cerveja gelada 🍺 Suco natural 🍹 Entre Nantes e
+        Iepê 📍
+      </Text>
+
+      <Text style={estilos.titulo}>RAIZ MUSIC BAR</Text>
+      <Image
+        source={require("../../assets/RANCHOBAR.jpg")}
+        style={estilos.principal}
+      />
+      <Text style={estilos.texto}>
+        RESTAURANTE PUP - Drinks/Cervejas/Porções
+      </Text>
+    </ScrollView>
   );
 }
 
@@ -16,17 +43,26 @@ const estilos = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#121212', 
+    backgroundColor: "#121212",
   },
   titulo: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#ffffff', 
+    fontWeight: "bold",
+    color: "#ffffff",
     marginBottom: 20,
+    textAlign: "center",
   },
   texto: {
     fontSize: 16,
-    color: '#e0e0e0', 
-    marginBottom: 10,
+    color: "#e0e0e0",
+    marginBottom: 50,
+    marginTop: 50,
+    textAlign: "justify",
+  },
+  principal: {
+    width: "100%",
+    height: undefined,
+    aspectRatio: 1,
+    borderRadius: 200,
   },
 });
